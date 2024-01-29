b13d233a6c9a62f607472669f6e555efe911bb3a
Ahmed samir al saed <ahmedshepl4242.com>
2024-01-29 22:47:47 +0200

    first g

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "https://numerical-methods-frontend.vercel.app/gaussElimination",
+      path: "/numerical-methods-frontend.vercel.app/gaussElimination",
       name: "gaussElimination",
       component: () => import("../components/methods/GaussElimination.vue"),
     },
     {
-      path: "https://numerical-methods-frontend.vercel.app/gaussJordan",
+      path: "/numerical-methods-frontend.vercel.app/gaussJordan",
       name: "gaussJordan",
 
       component: () => import("../components/methods/Gauss-Jordan.vue"),
@@ -39,74 +39,73 @@ const router = createRouter({
 export default router;
 
 //**************************extre methods ********************//
-    // {
-    //   path: "/conjugateGradient",
-    //   name: "conjugateGradient",
+// {
+//   path: "/conjugateGradient",
+//   name: "conjugateGradient",
 
-    //   component:() => import("../components/methods/ConjugateGradient.vue"),
-    // },
-    // {
-    //   path: "/newtonInterpolation",
-    //   name: "newtonInterpolation",
+//   component:() => import("../components/methods/ConjugateGradient.vue"),
+// },
+// {
+//   path: "/newtonInterpolation",
+//   name: "newtonInterpolation",
 
-    //   component:() => import("../components/methods/NewtonInterpolation.vue"),
-    // },
-    // {
-    //   path: "/lagrangeInterpolation",
-    //   name: "lagrangeInterpolation",
+//   component:() => import("../components/methods/NewtonInterpolation.vue"),
+// },
+// {
+//   path: "/lagrangeInterpolation",
+//   name: "lagrangeInterpolation",
 
-    //   component:() => import("../components/methods/LagrangeInterpolation.vue"),
-    // },
-    // {
-    //   path: "/splineInterpolation",
-    //   name: "splineInterpolation",
+//   component:() => import("../components/methods/LagrangeInterpolation.vue"),
+// },
+// {
+//   path: "/splineInterpolation",
+//   name: "splineInterpolation",
 
-    //   component:() => import("../components/methods/SplineInterpolation.vue"),
-    // },
-    // {
-    //   path: "/linearRegression",
-    //   name: "linearRegression",
+//   component:() => import("../components/methods/SplineInterpolation.vue"),
+// },
+// {
+//   path: "/linearRegression",
+//   name: "linearRegression",
 
-    //   component:() => import("../components/methods/LinearRegression.vue"),
-    // },
-    // {
-    //   path: "/polynomialRegression",
-    //   name: "polynomialRegression",
+//   component:() => import("../components/methods/LinearRegression.vue"),
+// },
+// {
+//   path: "/polynomialRegression",
+//   name: "polynomialRegression",
 
-    //   component:() => import("../components/methods/PolynomialRegression.vue"),
-    // },
-    // {
-    //   path: "/multipleLinearRegression",
-    //   name: "multipleLinearRegression",
+//   component:() => import("../components/methods/PolynomialRegression.vue"),
+// },
+// {
+//   path: "/multipleLinearRegression",
+//   name: "multipleLinearRegression",
 
-    //   component:() => import("../components/methods/MultipleLinearRegression.vue"),
-    // },
-    // {
-    //   path: "/nonlinearRegression",
-    //   name: "nonlinearRegression",
+//   component:() => import("../components/methods/MultipleLinearRegression.vue"),
+// },
+// {
+//   path: "/nonlinearRegression",
+//   name: "nonlinearRegression",
 
-    //   component:() => import("../components/methods/NonlinearRegression.vue"),
-    // },
-    // {
-    //   path: "/lagrangePolynomial",
-    //   name: "lagrangePolynomial",
+//   component:() => import("../components/methods/NonlinearRegression.vue"),
+// },
+// {
+//   path: "/lagrangePolynomial",
+//   name: "lagrangePolynomial",
 
-    //   component:() => import("../components/methods/LagrangePolynomial.vue"),
-    // },
-    // {
-    //   path: "/newtonPolynomial",
-    //   name: "newtonPolynomial",
+//   component:() => import("../components/methods/LagrangePolynomial.vue"),
+// },
+// {
+//   path: "/newtonPolynomial",
+//   name: "newtonPolynomial",
 
-    //   component:() => import("../components/methods/NewtonPolynomial.vue"),
-    // },
-    // {
-    //   path: "/linearSpline",
-    //   name: "linearSpline",
+//   component:() => import("../components/methods/NewtonPolynomial.vue"),
+// },
+// {
+//   path: "/linearSpline",
+//   name: "linearSpline",
 
-    //   component:() => import("../components/methods/LinearSpline.vue"),
-    // },
-    // {
-    //   path: "/quadraticSpline",
-    //   name: "quadraticSpline",
-    // }
-
+//   component:() => import("../components/methods/LinearSpline.vue"),
+// },
+// {
+//   path: "/quadraticSpline",
+//   name: "quadraticSpline",
+// }
